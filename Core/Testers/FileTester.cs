76d7cf2dc52280c7546609a4717474ef4b5f6e99
Dying <dyingvoid@gmail.com>
2023-04-05 19:48:15 +0500

    Last initial

---
+++ Core/Testers/FileTester.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using Core.Loggers;
+
+namespace Lab5WinterSemester.Core.Testers;
+
+public class FileTester : ITester<FileInfo>
+{
+    private ILogger _logger;
+    
+    public FileTester(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    public bool Test(FileInfo tableFile)
+    {
+        var data = File.ReadAllLines(tableFile.FullName);
+
+        var length = 0;
+        if (data.Length > 0)
+            length = data[0].Split(",").Length;
+
+        for (var i = 0; i < data.Length; ++i)
+        {
+            var strArrLength = data[i].Split(",").Length;
+            if (length != strArrLength)
+            {
+                _logger.Log($"File {tableFile.Name} with wrong dimensions. Found {i+1} row of size {strArrLength}, " +
+                            $"whereas 1st row of size {length}.\n");
+                
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ Lab6WinterSemester/Models/MainModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.ObjectModel;
+using System.IO;
+using Core.TableClasses;
+using Microsoft.Win32;
+
+namespace Lab6WinterSemester.Models;
+
+public class MainModel : IMainModel
+{
+    public ObservableCollection<DataBase> DataBases { get; set; }
+
+    public MainModel()
+    {
+        DataBases = new ObservableCollection<DataBase>();
+    }
+
+    public bool AddDataBase()
+    {
+        var file = OpenDataBaseFile();
+        if (file == null) 
+            return false;
+
+        DataBases.Add(CreateDataBase(file));
+        return true;
+    }
+
+    private FileInfo? OpenDataBaseFile()
+    {
+        var fileDialog = new OpenFileDialog();
+        if (fileDialog.ShowDialog() == true)
+        { 
+            return new FileInfo(fileDialog.FileName);
+        }
+
+        return null;
+    }
+    
+    private DataBase CreateDataBase(FileInfo dataBaseSchemaFile)
+    {
+        DataBaseSimpleFactory factory = new DataBaseSimpleFactory();
+        return factory.CreateDataBase(dataBaseSchemaFile);
+    }
+}
